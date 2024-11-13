f0e8254bf01bf0d8f5de18dff1fa10cab8b7657a
Mohit Jain <70198503+theGateway1@users.noreply.github.com>
2024-11-14 02:32:47 +0530

    Update postbuild.bash

--- postbuild.bash
+++ postbuild.bash
@@ -1,3 +1,5 @@
+# There was some issue with installation of Puppeteer on Heroku due to location of cache, so this is a fix for the same.
+
 echo -e "Store puppeteer executable in cache\n"
 
 mkdir ./.cache
